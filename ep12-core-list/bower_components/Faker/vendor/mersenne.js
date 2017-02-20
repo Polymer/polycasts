6e0ae5227bad9693f58f00152804adfca218c651
Rob Dodson <robdodson@google.com>
2017-02-20 09:56:10 -0800

    license headers

--- ep12-core-list/bower_components/Faker/vendor/mersenne.js
+++ ep12-core-list/bower_components/Faker/vendor/mersenne.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2017 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // this program is a JavaScript version of Mersenne Twister, with concealment and encapsulation in class,
 // an almost straight conversion from the original program, mt19937ar.c,
 // translated by y. okada on July 17, 2006.
